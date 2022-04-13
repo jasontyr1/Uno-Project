8b7aff6c023510e7588a9ffec339382f954f9c56
Lore <30599612+Lorenyx@users.noreply.github.com>
2022-04-13 08:53:50 -0500

    Init project

---
+++ src/AI.java
@@ -0,0 +1,13 @@
+/*
+extends player class to have a “name” and a deck
+Can do something with looking at the given cards in deck(Card array), give them points for how valid of a card it could be and send to card with the most/highest points to the discard deck. Up for interpretation on how to give out points to a card.
+methods:
+assignPointsToDeck(Card[] deck, Card cardOnTopOfDiscardStack)-look at the 			top card of discard deck and compare it each card in Players deck and assign it 			points given how choice of a choice to play it is. 
+use Player.removeCard(deck, cardIndex) to remove a card, put in on top of 			discard deck, and return new deck
+TODO figure out how I make an AI difficulty and what to change about the AI’s decisions given what difficulty is applied
+
+*/
+class AI extends Player {
+    void assignPointsToDeck(Card[] deck, Card cardOnTopOfDiscardStack);
+    void removeCard(deck, cardIndex);
+}
---
+++ src/BoardController.java
@@ -0,0 +1,3 @@
+/*
+Board class(main) – This will show the scene, create and assign players, set difficulty, start the game, control whos turn it is(on start of turn, check player hand to see if they actually have a valid move), hold discard deck Card array,  display players cards, display top discard deck card, and make it look pretty it some ways(scoreboard or something)
+*/
