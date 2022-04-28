43a02ea82292de8326393429cf29ddf225ec11a8
Lore <30599612+Lorenyx@users.noreply.github.com>
2022-04-27 20:21:41 -0500

    Fixed unicode string

--- src/application/AI.java
+++ src/application/AI.java
@@ -1,6 +1,6 @@
 package application;
 /*
-extends player class to have a “name” and a deck
+extends player class to have a "name" and a deck
 Can do something with looking at the given cards in deck(Card array), give them points for how valid of a card it could be and send to card with the most/highest points to the discard deck. Up for interpretation on how to give out points to a card.
 methods:
 assignPointsToDeck(Card[] deck, Card cardOnTopOfDiscardStack)-look at the 			top card of discard deck and compare it each card in Players deck and assign it 			points given how choice of a choice to play it is. 
