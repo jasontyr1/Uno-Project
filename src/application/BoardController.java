29c7ec157d89367df557a49ae75326f5260914b4
Lore <30599612+Lorenyx@users.noreply.github.com>
2022-05-02 15:57:27 -0500

    Uploaded Boards

--- src/application/BoardController.java
+++ src/application/BoardController.java
@@ -1,23 +1,30 @@
 package application;
 
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.effect.BlurType;
+import javafx.scene.effect.BoxBlur;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 
 public class BoardController {
 	
 	Draggable drag = new Draggable();//Object to make player cards draggable
 	DropShadow hoverShadow = new DropShadow(); //Object for the glowing border on the player cards
+	BoxBlur blur = new BoxBlur();
 	private double temp; // Holds the original xLayout when dragging the playerCard
 	// Will hold the original x and y coordinates of the player cards
 	private double x1, x2, x3, x4, x5, x6,x7;
 	private double y1, y2, y3, y4, y5, y6,y7;
+	int unoTemp = 1;
 
 	@FXML
 	private ImageView card;
@@ -38,13 +45,30 @@ public class BoardController {
 	@FXML
 	private ImageView DiscardPile;
 	@FXML
+	private ImageView deck;
+	@FXML
+	private Button drawButton;
+	@FXML
 	private AnchorPane pane;
+	@FXML
+    private Region colorSelector;
+	@FXML
+	private Button greenButton;
+	@FXML
+	private Button redButton;
+	@FXML
+	private Button blueButton;
+	@FXML
+	private Button yellowButton;
+	@FXML
+	private Button unoButton;
 	
 	
 	
 	//Gets the original X and Y Layout of the players Cards
 	//Needed for making sure moving pieces can go back to original places when needed
 	public void initialize() {
+		
 		x1 = card1.getLayoutX();
 		y1 = card1.getLayoutY();		
 		x2 = card2.getLayoutX();
@@ -59,6 +83,16 @@ public class BoardController {
 		y6 = card6.getLayoutY();
 		x7 = card7.getLayoutX();
 		y7 = card7.getLayoutY();
+		
+		hoverShadow.setBlurType(BlurType.GAUSSIAN);
+		hoverShadow.setColor(Color.WHITE);
+		hoverShadow.setSpread(.90);
+		
+		System.out.println(DiscardPile.getImage());
+		if(DiscardPile.getImage().toString() == "WILD.jpg")
+		{
+			System.out.println(DiscardPile.getImage().toString());
+		}
 	}
 	
 	public void onCardHover(MouseEvent event) {
@@ -72,9 +106,7 @@ public class BoardController {
 		//Test if the card is touching the discard pile
 		if(!(card.getBoundsInParent().intersects(DiscardPile.getBoundsInParent()))) {
 			//Used to create outline effect for the card being hovered over
-			hoverShadow.setBlurType(BlurType.GAUSSIAN);
-			hoverShadow.setColor(Color.WHITE);
-			hoverShadow.setSpread(.90);
+
 		
 			//Used to make card able to be dragged
 			drag.makeDraggable(card);
@@ -101,48 +133,81 @@ public class BoardController {
 	public void cardDropped(MouseEvent event) {
 		//Test if the card is touching the discard pile and if the image on the card is blank
 		if(card.getBoundsInParent().intersects(DiscardPile.getBoundsInParent()) && (card.getImage() != null)) {
-			//sets the image of the discard pile to that of the card played
-			DiscardPile.setImage(card.getImage());
-			//sets them image of the card played to null;
-			card.setImage(null);
 			
-			//Shifts the cards down when a card is played so there is no gaps
-			if(card1.getImage() == null ) {
-				card1.setImage(card2.getImage());
-				card2.setImage(null);
-			}
-			if(card2.getImage() == null) {
-				card2.setImage(card3.getImage());
-				card3.setImage(null);
-			}
-			if(card3.getImage() == null) {
-				card3.setImage(card4.getImage());
-				card4.setImage(null);
-			}
-			if(card4.getImage() == null) {
-				card4.setImage(card5.getImage());
-				card5.setImage(null);
-			}
-			if(card5.getImage() == null) {
-				card5.setImage(card6.getImage());
-				card6.setImage(null);
-			}
-			if(card6.getImage() == null) {
-				card6.setImage(card7.getImage());
-				card7.setImage(null);
-			}
+			DiscardPile.setImage(card.getImage());//sets the image of the discard pile to that of the card played
+			card.setImage(null);//sets them image of the card played to null;
 			
+			cardShift();//Call to card shift to shift the cards down when a card is played
+			
+			if(false)//if the card will call on the to string to test in the card name is WILD.jpg
+			{
+				colorSelector();
+			}
+			if(true)//Check to see if the amount of cards in the players hand is set to 1
+			{
+				unoButton.setVisible(true);//Set unoButton to be visible
+				//Creates a pause transition lasting 2 seconds
+				PauseTransition visiblePause = new PauseTransition(Duration.seconds(2)); 
+				//After pause transition finishes lambda expressions checks if the card was clicked or not
+				visiblePause.setOnFinished(event2 ->{ 
+					unoButton.setVisible(false);
+					if(unoTemp != 0) {
+						//Add cards
+						unoTemp = 1;
+						System.out.println("UNO wasn't pressed");
+					}
+					else {
+						unoTemp = 1;
+						System.out.println("Uno Pressed");
+					}
+				});
+				visiblePause.play();
+			}
 		}
-		
 		//Call to fixLayout
 		fixLayout();
+	}
+	
+	
+	public void onUnoClicked(MouseEvent event) {
+		unoButton.setVisible(false);
+		unoTemp = 0;
+	}
+	
+	public void colorSelector() {
+		DiscardPile.setEffect(blur);
+		deck.setEffect(blur);
+		drawButton.setEffect(blur);
+		
+		colorSelector.setVisible(true);
+		redButton.setVisible(true);
+		blueButton.setVisible(true);
+		greenButton.setVisible(true);
+		yellowButton.setVisible(true);
+	}
+	
+	public void onColorButton(MouseEvent event) {
+		Button colorButton = (Button) event.getSource();
+		
+		DiscardPile.setEffect(null);
+		deck.setEffect(null);
+		drawButton.setEffect(null);
+		
+		colorSelector.setVisible(false);
+		redButton.setVisible(false);
+		blueButton.setVisible(false);
+		greenButton.setVisible(false);
+		yellowButton.setVisible(false);
+		
+		System.out.println(colorButton.getId());
+		
 	}
 	
 	public void drawCard(MouseEvent event) {
 		
 		//sets image of the card drawn from deck will be dynamic after full implementation
-		Image image = new Image(getClass().getResource("data/test.jpg").toExternalForm());
-		// image = new Image(getClass().getResourceAsStream("data/test.jpg"));
+		Image image = new Image(getClass().getResource("data/RED_REVERSE.png").toExternalForm());
+		
 		//Sets the image of the first blank card it finds when the draw card is clicked
 		if(card1.getImage() == null) {
 			card1.setImage(image);
@@ -169,6 +234,35 @@ public class BoardController {
 		//Call to fixLayout
 		fixLayout();
 		
+	}
+	
+	public void cardShift() {
+				
+				//Shifts the cards down when a card is played so there is no gaps
+				if(card1.getImage() == null ) {
+					card1.setImage(card2.getImage());
+					card2.setImage(null);
+				}
+				if(card2.getImage() == null) {
+					card2.setImage(card3.getImage());
+					card3.setImage(null);
+				}
+				if(card3.getImage() == null) {
+					card3.setImage(card4.getImage());
+					card4.setImage(null);
+				}
+				if(card4.getImage() == null) {
+					card4.setImage(card5.getImage());
+					card5.setImage(null);
+				}
+				if(card5.getImage() == null) {
+					card5.setImage(card6.getImage());
+					card6.setImage(null);
+				}
+				if(card6.getImage() == null) {
+					card6.setImage(card7.getImage());
+					card7.setImage(null);
+				}
 	}
 	
 	//Function is used to keep the layout correct to make sure no bugs mess it up
